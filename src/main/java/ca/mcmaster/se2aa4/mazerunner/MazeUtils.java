3296337515115fb68939033ea7a8155fb6ecf0ff
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2025-01-20 02:06:53 +0000

    forgot this .. now would be ready for implementation

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeUtils.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class MazeUtils {
+    public static String toCanonicalPath(String path) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }    
+
+    public static String toFactoredPath(String path) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
