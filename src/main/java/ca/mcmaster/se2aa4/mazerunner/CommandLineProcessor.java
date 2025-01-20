5fa2c2b52d03051410294907d62a9dc1cb998826
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2025-01-20 01:39:51 +0000

    made a quick fix

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/CommandLineProcessor.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/CommandLineProcessor.java
@@ -1,8 +1,10 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import org.apache.commons.cli.CommandLine;
+
 public class CommandLineProcessor {
 
-    public void run(String[] args) {
+    public CommandLine run(String[] args) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
